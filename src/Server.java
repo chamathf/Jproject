554b47211a9584ac506b18d7aa1f00586d05f23b
Chamath Fernando <malaka7fernando@gmail.com>
2016-04-14 18:48:48 +0530

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+public class Server {
+
+	public static void main(String[] args)throws InterruptedIOException {
+		// TODO Auto-generated method stub
+ServerSocket S=new Server Socket(5000);
+Socket S1=S.accept();
+outputStream S1 out=S1.get outputStream();
+Socket S1=S.accept();
+
+outputStream S1 out=S1.getoutput Stream();
+DataInput Stream dos=new Dataoutput Stream(S1.out);
+do.Write UTF("Hi there");
+dos.close();
+S1.out.close();
+S1.Close();
+	}
+
+
+
+	}
+
+}
